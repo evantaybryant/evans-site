5131fb8aa9541f06acfb80ed23f54ad65360d9c6
evan <evantaybryant@yahoo.com>
2021-04-16 20:27:05 -0600

    addedexclamation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
          <h1>Evan's Site</h1>
-         <p>Hi, my name is Evan - I'm learning how to code.</p>
+         <p>Hi, my name is Evan - I'm learning how to code!</p>
       </header>
     </div>
   );
