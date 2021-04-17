9115198da7a31d04af61de5ebf762d4ca4a587ab
evan <evantaybryant@yahoo.com>
2021-04-16 20:17:06 -0600

    my first ever commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+         <h1>Evan's Site</h1>
+         <p>Hi, my name is Evan - I'm learning how to code.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
